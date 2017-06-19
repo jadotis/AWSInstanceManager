76b0be54cd4c408e7d373dfe61632b52e6ebdbcd
James Otis <jadotis@bu.edu>
2017-06-19 09:32:34 -0400

    Bug Fixes and Functionality
    -Added Clear Screen to avoid Clutter upon execution
    -Added flags to keep track of variable Statuses
    -Added sleep functionality to keep track of instances while they start/stop
    -Added detailed logging in neat format

--- awsScript.py
+++ awsScript.py
@@ -1,9 +1,13 @@
-#usr/bin/python
+#!usr/bin/env python
+#James Otis#
 
 import sys
 import json
 import boto3
+import time
+import os
 
+os.system("cls" if os.name == 'nt' else 'clear')
 def getInstance(instanceID):
 	'''
 	gets a specified instance from the cloud
@@ -15,8 +19,7 @@ def getInstance(instanceID):
     	],
     		DryRun = False
     	  	)
-	print(response['Reservations'][0]['Instances'])
-
+	return response['Reservations'][0]['Instances'][0]["State"]["Name"]
 
 def stopInstances(instanceList):
 	'''
@@ -30,7 +33,7 @@ def stopInstances(instanceList):
     			InstanceIds=[
         			i,
     			],
-    				DryRun = True
+    				DryRun = False
 			)	
 		results.append("Status of instance: " + str(i) +": " + str(response["StoppingInstances"][0]["CurrentState"]["Name"]))  
 	print("The resulting Statuses: " + str(results))	
@@ -41,19 +44,15 @@ def startInstances(instanceList):
 	'''
 	print("starting specified instances")
 	results = []
-	
 	for i in instanceList:
 		response = client.start_instances(
 			InstanceIds=[
 				i,
 			],
-				DryRun = True
+				DryRun = False
 			)
-		results.append("Status of the instance: " + str(i) + ": " + str(response["StartingInstances"][0]["CurrentState"]["Name"]))
- 
+		results.append("Status of the instance: " + str(i) + ": " + str(response["StartingInstances"][0]["CurrentState"]["Name"])) 
 	print("The resulting Statuses: " + str(results))
-
-
 
 #####BODY#####
 
@@ -61,8 +60,6 @@ print("Please make sure that your instance IDs are space delimited")
 print("EX: python <scriptName.py> 'Start/Stop' instance1 instance2 instance3")
 with open('credentials.json') as data_file:
 	data = json.load(data_file)
-
-
 
 ####Authentication to AWS####
 ACCESS_KEY = data["ACCESS_KEY"]
@@ -90,12 +87,36 @@ i = 2
 while(len(sys.argv) > i):
 	instances.append(str(sys.argv[i]))
 	i += 1
-print(instances)
-
 
 #####Function Calls#####
+desiredStatus = ""
 if str(sys.argv[1].lower()) == "stop":
+	desiredStatus = "stopped"
 	stopInstances(instances)
 else:
+	desiredStatus = "started"
 	startInstances(instances)
 
+
+#####Logging and Status Checks#####
+print("Script will now sleep to confirm that the instances were started/stopped")
+print("Stopping the script will not affect the changes to the instances")
+flag = 0
+wait = 60
+while(time > 0 and  flag != len(instances)):
+	for i in instances:
+		status = getInstance(i)
+		if status == "stopped" or status == "running":
+			flag += 1
+			instances.remove(i)
+		print("The status for instance: " + str(i) + " is: " + str(getInstance(i)))
+	time.sleep(5)
+	print("-------------------------------------------------------")
+	wait -= 5
+
+if time == 0:
+	print("There was an error stopping the specified instances")
+else:
+	
+	for i in instances:
+		print("Success, all instances are " + desiredStatus + ".\nTerminating..." ) 
